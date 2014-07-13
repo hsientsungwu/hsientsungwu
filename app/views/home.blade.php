dbe6a3f638dc5e820c516d75ccdeba5e75c78dac
hsientsungwu <hwu1986@gmail.com>
2014-07-13 11:34:00 -0400

    portfolio and footer

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -5,58 +5,46 @@
 @stop
 
 @section('content')
-	<div class="container top-posts">
+
+    <div class="container top-portfolios">
         <div class="row">
-            <div class="col-sm-4 section">
-                <h1 class="section-heading">Top Post #1</h1>
-                <p class="lead section-lead">This stylish template features placeholder images from placehold.it and lorempixel! With some custom font stylings and awesome photos, you will have a beautiful website up and running in no time!</p>
-                <p class="section-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sollicitudin auctor quam ac tempor. Cras a ante sed libero mollis sodales. Praesent fringilla, neque ut ultrices faucibus, dolor eros ultrices neque, nec bibendum arcu ipsum eget justo. Phasellus vestibulum sagittis purus laoreet varius. Pellentesque malesuada malesuada mattis. Aliquam sed porta nisi, eget suscipit dolor. Nam ipsum sapien, rhoncus eu leo eu, ultricies pellentesque tellus.</p>
+            <div class="col-lg-12 text-center section-header">
+                <h2>Portfolio</h2>
+                <hr class="star-primary">
             </div>
-            <div class="col-sm-4 section">
-                <h1 class="section-heading">Top Post #2</h1>
-                <p class="lead section-lead">This stylish template features placeholder images from placehold.it and lorempixel! With some custom font stylings and awesome photos, you will have a beautiful website up and running in no time!</p>
-                <p class="section-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sollicitudin auctor quam ac tempor. Cras a ante sed libero mollis sodales. Praesent fringilla, neque ut ultrices faucibus, dolor eros ultrices neque, nec bibendum arcu ipsum eget justo. Phasellus vestibulum sagittis purus laoreet varius. Pellentesque malesuada malesuada mattis. Aliquam sed porta nisi, eget suscipit dolor. Nam ipsum sapien, rhoncus eu leo eu, ultricies pellentesque tellus.</p>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-4 portfolio-item">
+                <div class="thumbnail">
+                    <img src="/img/portfolio/mabow.png" alt="mabow" class="img-responsive">
+                    <div class="caption">
+                        <a href="https://github.com/hsientsungwu/mabow" target="_blank"><h3>Mawbow - 媽寶</h3></a>
+                        <p>Mabow is a web application that crawls Youtube videos and categorize them into users' needs to maximize user experience. </p>
+                    </div>
+                </div>
             </div>
-            <div class="col-sm-4 section">
-                <h1 class="section-heading">Top Post #3</h1>
-                <p class="lead section-lead">This stylish template features placeholder images from placehold.it and lorempixel! With some custom font stylings and awesome photos, you will have a beautiful website up and running in no time!</p>
-                <p class="section-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sollicitudin auctor quam ac tempor. Cras a ante sed libero mollis sodales. Praesent fringilla, neque ut ultrices faucibus, dolor eros ultrices neque, nec bibendum arcu ipsum eget justo. Phasellus vestibulum sagittis purus laoreet varius. Pellentesque malesuada malesuada mattis. Aliquam sed porta nisi, eget suscipit dolor. Nam ipsum sapien, rhoncus eu leo eu, ultricies pellentesque tellus.</p>
+
+            <div class="col-sm-4 portfolio-item">
+                <div class="thumbnail">
+                    <img src="/img/portfolio/transmatter.png" alt="mabow" class="img-responsive">
+                    <div class="caption">
+                        <a href="https://github.com/hsientsungwu/Transmatter/" target="_blank"><h3>Transmatter</h3></a>
+                        <p>Transmatter provides the instant search platform from various dictionary databases. It focuses on translation from Chinese (Madarin) to English.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-4 portfolio-item">
+                <div class="thumbnail">
+                    <img src="/img/portfolio/usftsa.png" alt="mabow" class="img-responsive">
+                    <div class="caption">
+                        <a href="http://www.usftsa.com" target="_blank"><h3>USFTSA.COM</h3></a>
+                        <p>Student Organization website that features e-news, sponsors, timeline history, survey forms, and various useful functionality for student leader group.</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 
-    <div class="container top-portfolios">
-        <div class="row">
-            <div class="col-sm-4 portfolio-item">
-                <a href="#portfolioModal1" class="portfolio-link" data-toggle="modal">
-                    <div class="caption">
-                        <div class="caption-content">
-                            <i class="fa fa-search-plus fa-3x"></i>
-                        </div>
-                    </div>
-                    <img src="img/portfolio/cabin.png" class="img-responsive" alt="" />
-                </a>
-            </div>
-            <div class="col-sm-4 portfolio-item">
-                <a href="#portfolioModal2" class="portfolio-link" data-toggle="modal">
-                    <div class="caption">
-                        <div class="caption-content">
-                            <i class="fa fa-search-plus fa-3x"></i>
-                        </div>
-                    </div>
-                    <img src="img/portfolio/cake.png" class="img-responsive" alt="" />
-                </a>
-            </div>
-            <div class="col-sm-4 portfolio-item">
-                <a href="#portfolioModal3" class="portfolio-link" data-toggle="modal">
-                    <div class="caption">
-                        <div class="caption-content">
-                            <i class="fa fa-search-plus fa-3x"></i>
-                        </div>
-                    </div>
-                    <img src="img/portfolio/circus.png" class="img-responsive" alt="" />
-                </a>
-            </div>
-        </div>
-    </div>
 @stop
--- app/views/template/master.blade.php
+++ app/views/template/master.blade.php
@@ -1,7 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
+<!DOCTYPE html 
+      PUBLIC "-//W3C//DTD HTML 4.01//EN"
+      "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en-US">
+<head profile="http://www.w3.org/2005/10/profile">
+    <link rel="icon" type="image/png" href="favicon.ico">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
@@ -14,6 +16,8 @@
 
     <!-- Custom CSS for the 'Full Width Pics' Template -->
     <link href="/css/full-width-pics.css" rel="stylesheet">
+    <link href="/css/custom.css" rel="stylesheet">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
 
     @yield('head')
 </head>
@@ -35,9 +39,9 @@
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse navbar-ex1-collapse">
                 <ul class="nav navbar-nav">
-                    <li><a href="/blog/">Blog</a>
+                    <li><a href="/#blog">Blog</a>
                     </li>
-                    <li><a href="/contact/">Contact</a>
+                    <li><a href="/#contact">Contact</a>
                     </li>
                 </ul>
             </div>
@@ -48,30 +52,33 @@
 
     <div class="full-width-image-1">
 
-        <div class="logo-wrapper">
-            <img class="img-responsive" src="http://placehold.it/200x200&text=Logo" />
+        <div class="circular logo-wrapper"></div>
+        <div class="social-wrapper">
+            <a href="http://twitter.com/HsienTsungWu" target="_blank"><i class="fa fa-twitter-square fa-3x"></i></a>
+            <a href="http://facebook.com/limitles5" target="_blank"><i class="fa fa-facebook-square fa-3x"></i></a>
+            <a href="http://github.com/hsientsungwu" target="_blank"><i class="fa fa-github-square fa-3x"></i></a>
         </div>
 
     </div>
     
-    @yield('content')
+    <div class="content">
+        @yield('content')
+    </div>
 
     <!-- FOOTER -->
-    <div class="container">
-        <footer>
-            <div class="row">
+    <div class="footer">
+        <div class="container">
+            <div class="row footer-section">
                 <div class="col-lg-12">
                     Copyright &copy; HSIENTSUNGWU.COM 2014
                 </div>
             </div>
-        </footer>
-    </div>
-
+        </div>
     </div>
     <!-- /container -->
 
     <!-- JavaScript -->
-    <script src="/packages/bootstrap/js/jquery-1.10.2.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" ></script>
     <script src="/packages/bootstrap/js/bootstrap.js"></script>
 
     @yield('scripts')
