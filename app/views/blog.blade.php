82fba9c94f5df93ae7134e5ac02af9500f93e402
hsientsungwu <hwu1986@gmail.com>
2014-07-16 00:05:38 -0400

    footer optimization

--- app/views/blog.blade.php
+++ app/views/blog.blade.php
@@ -18,7 +18,7 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-md-8 col-md-offset-2 post-pagination">
                 {{ $posts->links() }}
             </div>
         </div>
