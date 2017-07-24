dba95b1214b8e9066fe6b90128494159f4276995
Steve Wu <hwu1986@gmail.com>
2017-07-23 21:30:01 -0400

    test 4

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,35 +26,24 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        /*
+
         $schedule->call(function () {
-            $response = Trip::hasTripArrivedAtStop('584', 'Wellesley Square');
+            $alerts = CommuterAlert::where('alertType', '=', 1);
 
-            $alert = CommuterAlert::where('alertId', '=', 1)->first();
-
-            $alert->notify($response);
+            foreach ($alerts as $alert) {
+                $response = Trip::hasTripArrivedAtStop($alert->alertTripId, $alert->alertStopId);
+                $alert->notify($response);
+            }
         })->everyMinute()->between("06:30", "07:10");
-*/
-        $schedule->call(function () {
-            $response = Trip::hasTripArrivedAtStop('2514', 'Framingham');
 
-            $alert = CommuterAlert::where('alertId', '=', 3)->first();
-            $alert->notify($response);
+        $schedule->call(function () {
+            $alerts = CommuterAlert::where('alertType', '=', 2);
+
+            foreach ($alerts as $alert) {
+                $response = Trip::hasTripArrivedAtStop($alert->alertTripId, $alert->alertStopId);
+                $alert->notify($response);
+            }
         })->everyMinute();
-
-        $schedule->call(function () {
-            $data = "payload=" . json_encode(array(
-                    "text"          =>  "testing - " . time()
-                ));
-
-            // You can get your webhook endpoint from your Slack settings
-            $ch = curl_init("https://hooks.slack.com/services/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");
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            $result = curl_exec($ch);
-            curl_close($ch);
-        })->everyFiveMinutes();
     }
 
     /**
